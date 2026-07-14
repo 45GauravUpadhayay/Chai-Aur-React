4e6e1a665245f2442fec659e3be81320407d9a67
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-07-14 14:29:51 +0530

    Project Done

--- 01_Counter_Project/src/App.jsx
+++ 01_Counter_Project/src/App.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
+import { useState } from 'react';
 import "./index.css";
 
 const App = () => {
+
+  let [Counter, setCounter] = useState(0);
+
+  // let Counter = 5;
+
+  const addValue = () => {
+    console.log("Clicked", Counter);
+    Counter = Counter + 1;
+    setCounter(Counter)
+    console.log("Clicked", Counter);
+  }
+
+
+  const RemoveValue = () => {
+    setCounter(Counter - 1)
+  }
+    
+
   return (
     <>
       <div id='CounterProject'>
         <h1>Chai Aur React</h1>
-        <h2>Counter Value: 5</h2>
+        <h2>Counter Value: {Counter}</h2>
+
         <div className="Button">
-          <button className='AddBtn'>Add Value</button>
-          <button className='RemoveBtn'>Remove Value</button>
+
+          <button className='AddBtn'
+          onClick={addValue}>Add Value</button>
+
+          <button className='RemoveBtn'
+          onClick={RemoveValue}>Remove Value</button>
+
         </div>
+
         </div>
     </>
   )
