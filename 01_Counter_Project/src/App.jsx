c0e121880082db257c67731dd741e38dc39752da
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-07-14 13:54:01 +0530

    Css Complete

---
+++ 01_Counter_Project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <div id='CounterProject'>
+        <h1>Chai Aur React</h1>
+        <h2>Counter Value: 5</h2>
+        <div className="Button">
+          <button className='AddBtn'>Add Value</button>
+          <button className='RemoveBtn'>Remove Value</button>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default App
