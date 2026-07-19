70a8cd2ad46140694efe3aa7c5b709b9cf2fcde0
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-07-19 14:35:52 +0530

    set up

--- 01_Counter_Project/src/App.jsx
+++ 01_Counter_Project/src/App.jsx
@@ -1,42 +1,33 @@
 import React from 'react'
 import { useState } from 'react';
-import "./index.css";
+import './index.css'
 
 const App = () => {
 
-  let [Counter, setCounter] = useState(0);
+    let [Counter , setCounter] = useState(0);
 
-  const addValue = () => {
-    if(Counter < 20){
-    Counter = Counter + 1;
-    setCounter(Counter)
+    const addValue = () => {
+        if(Counter < 20){
+            Counter = Counter + 1;
+            setCounter(Counter);
+        }
     }
-  }
 
-  const RemoveValue = () => {
-    if(Counter > 0) setCounter(Counter - 1)
-  }
+    const removeValue = () => {
+        if(Counter > 0) setCounter(Counter-1)
+    }
 
 
-  console.log(Counter)
-    
 
   return (
     <>
-      <div id='CounterProject'>
-        <h1>Chai Aur React</h1>
-        <h2>Counter Value: {Counter}</h2>
-
-        <div className="Button">
-
-          <button className='AddBtn'
-          onClick={addValue}>Add Value</button>
-
-          <button className='RemoveBtn'
-          onClick={RemoveValue}>Remove Value</button>
-
-        </div>
-
+        <div className="counterApp">
+            <h1 className='mainHeading'>Counter App</h1>
+            <h3 className='smallHeading'>Count: {Counter}</h3>
+            <div className="buttons">
+                <button className='btn1'  onClick={addValue}>Add Value</button>
+                <button className='btn2' onClick={removeValue}>Remove Value</button>
+            </div>
         </div>
     </>
   )
